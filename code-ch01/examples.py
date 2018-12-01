e5dd8667dba7b2d09634bcc62c98c9c0fd13ba17
Jimmy Song <jaejoon@gmail.com>
2018-12-01 11:34:11 -0600

    more exp

---
+++ code-ch01/examples.py
@@ -0,0 +1,17 @@
+"""
+# tag::example1[]
+>>> from ecc import FieldElement
+>>> a = FieldElement(7, 13)
+>>> b = FieldElement(6, 13)
+>>> print(a == b)
+False
+>>> print(a == a)
+True
+# end::example1[]
+"""
+"""
+# tag::example2[]
+>>> print(7 % 3)
+1
+# end::example2[]
+"""
