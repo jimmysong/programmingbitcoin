782c113aba1906d8341c89180de38f99714cd79a
Jimmy Song <jaejoon@gmail.com>
2018-12-02 21:15:54 -0600

    corrections

--- code-ch04/answers.py
+++ code-ch04/answers.py
@@ -67,7 +67,8 @@ Find the DER format for a signature whose `r` and `s` values are:
 >>> s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
 >>> sig = Signature(r,s)
 >>> print(sig.der().hex())
-3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeae
+c0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
 
 # end::answer3[]
 # tag::exercise4[]
