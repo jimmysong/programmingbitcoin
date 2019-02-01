756d9dedba5f28674d8142d0cb7537860b5a2895
dbaker <dbaker@oreilly.com>
2019-02-01 07:56:46 -0800

    Edited code-ch10/answers.py with Atlas code editor

--- code-ch10/answers.py
+++ code-ch10/answers.py
@@ -9,7 +9,7 @@ Determine what this network message is:
 # tag::answer2[]
 >>> from network import NetworkEnvelope
 >>> from io import BytesIO
->>> message_hex = 'f9beb4d976657261636b000000000000000000005df6e0e2'
+>>> message_hex = `f9beb4d976657261636b000000000000000000005df6e0e2`
 >>> stream = BytesIO(bytes.fromhex(message_hex))
 >>> envelope = NetworkEnvelope.parse(stream)
 >>> print(envelope.command)
