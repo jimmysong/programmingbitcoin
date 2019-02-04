5cc713329ae622903fcd670d3a240d138f2f18f9
kristen@oreilly.com <kristen@oreilly.com>
2019-02-04 08:00:44 -0800

    Edited code-ch04/answers.py with Atlas code editor

--- code-ch04/answers.py
+++ code-ch04/answers.py
@@ -51,9 +51,11 @@ Find the compressed SEC format for the public key where the private key secrets 
 
 Find the DER format for a signature whose `r` and `s` values are:
 
-* `r` = pass:[<br/>]`0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6`
+----
+r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
 
-* `s` = pass:[<br/>]`0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec`
+s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+----
 # end::exercise3[]
 # tag::answer3[]
 >>> from ecc import Signature
